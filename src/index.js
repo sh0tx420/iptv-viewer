bac99de88bc59eb19463c8f4a2facd713dd5136e
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-04-28 13:31:57 +0300

    Added code for loading pages and CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// h
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./Home";
+import "./index.css"; // webpack automagically loads this
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <Home/>
+);
