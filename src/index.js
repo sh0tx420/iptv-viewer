3b5eefd07831a5b41b1d8ec51dbb03d16bbfff11
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-05-08 22:17:33 +0300

    Added theming

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./Home";
+import { ThemeProvider } from "@emotion/react";
 import "./index.css"; // webpack automagically loads this
+import theme from "./theme";
 
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <Home/>
+    <ThemeProvider theme={theme}>
+        <Home/>
+    </ThemeProvider>
 );
